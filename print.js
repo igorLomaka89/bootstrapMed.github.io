439e811ee93a149e94bfe6159b773fd9e3babf68
igorLomaka89 <157588362+igorLomaka89@users.noreply.github.com>
2024-05-24 09:28:54 +0300

    Add files via upload

---
+++ print.js
@@ -0,0 +1,6 @@
+// // document.querySelector('.submit').onclick = myClick;
+// document.querySelector('.submit').addEventListener('click', myClick)
+// function myClick() {
+//     let a = document.querySelector('.i-1').value;
+//     document.querySelector('.i-2').innerHTML = a
+// }
